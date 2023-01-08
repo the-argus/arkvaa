cd7e42551495213545af38de6d228a65512a2d75
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-08 12:18:18 -0600

    add gitignore and flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Documents relating to Arkv'aa Desert Tavern";
+  inputs.nixpkgs.url = github:nixos/nixpkgs?ref=nixos-22.11;
+  inputs.lonesome-stranger.url = github:the-argus/lonesome-stranger;
+
+  outputs = {
+    self,
+    nixpkgs,
+    lonesome-stranger,
+  }: let
+    supportedSystems = [
+      "x86_64-linux"
+      "aarch64-linux"
+    ];
+    genSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgs = genSystems (system: import nixpkgs {inherit system;});
+  in {
+    devShell = genSystems (system:
+      pkgs.${system}.mkShell {
+        packages = [
+          (pkgs.${system}.python310.withPackages (_: [
+            lonesome-stranger.packages.${system}.dungeonsheets
+          ]))
+        ];
+      });
+  };
+}
